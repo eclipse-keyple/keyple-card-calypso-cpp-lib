85becf81e382865a2059b66fc0bff16515da4192
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2023-01-03 19:26:08 +0100

    ci: improve doxygen doc publication process (#7)
    * build: prepare next release
    
    * ci: improve doxygen doc publication process

--- .github/scripts/patch_doxyfile.sh
+++ .github/scripts/patch_doxyfile.sh
@@ -2,11 +2,6 @@
 
 project_version=$1
 
-if [ "$project_version" = "" ]; then
-    echo "ERROR: project version is undefined"
-    exit 1
-fi
-
 sed -i "s/%PROJECT_VERSION%/$project_version/g" ./.github/doxygen/Doxyfile
 
 project_name="$(head -n 1 README.md | sed 's/#//')"
